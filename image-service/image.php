efee728779ab05392568e3185a9df8be99d0daef
Yves Paul Cayaco <ypaulc@gmail.com>
2018-03-13 00:36:00 +0800

    Accept date, apply as seed.

--- image-service/image.php
+++ image-service/image.php
@@ -1,10 +1,12 @@
 <?php
 $width = 4;
 $height = 4;
+$date = strtotime($_GET['date']);
 
 $image = imagecreatetruecolor($width, $height);
 for($row = 1; $row <= $height; $row++) {
     for($column = 1; $column <= $width; $column++) {
+	mt_srand($date);
         $red = mt_rand(0,255);
         $green = mt_rand(0,255);
         $blue = mt_rand(0,255);
